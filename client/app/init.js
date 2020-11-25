daca500727bf485e55706f270ca62697f04813da
Mamé <mameazad.turgut@gmail.com>
2020-11-25 19:33:28 +0100

    search by name

--- client/app/init.js
+++ client/app/init.js
@@ -5,6 +5,7 @@ import { leaveComment } from './handlers/leave_comment.js';
 import { nightMode } from './handlers/nightmode.js';
 import { editComment } from './handlers/edit-comment.js';
 import { deleteComment } from './handlers/delete.js';
+import { searchByName } from './handlers/search.js';
 
 /*change avatarimages*/
 document.getElementById('my-select').addEventListener('change', setimage);
@@ -35,7 +36,10 @@ var checkbox = document.querySelector('input[name=mode]');
 checkbox.addEventListener('change', nightMode);
 
 /*edit comment*/
-document.getElementById('chat').addEventListener('click',editComment);
+document.getElementById('chat').addEventListener('click', editComment);
 
 /* delete comment*/
-document.getElementById('chat').addEventListener('click',deleteComment);
+document.getElementById('chat').addEventListener('click', deleteComment);
+
+/*Search By Name */
+document.getElementById('serchByName').addEventListener('keyup', searchByName);
