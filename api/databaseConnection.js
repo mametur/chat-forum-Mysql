13ac2dae63d94ca4f15bfde6ce9807a85a7b457f
Mamé <mameazad.turgut@gmail.com>
2020-12-01 14:40:48 +0100

    add complete readme

--- api/databaseConnection.js
+++ api/databaseConnection.js
@@ -1,9 +1,13 @@
 mysql = require('mysql');
 
-db = mysql.createPool({
-	host: 'eu-cdbr-west-03.cleardb.net',
-	user: 'bb6c73c3a5e0e2',
-	password: 'd4d70813',
-	database: 'heroku_16ab16187995650',
+db = mysql.createConnection({
+	host: 'localhost',
+
+	user: 'root',
+
+	password: 'password',
+
+	database: 'chatdb',
 });
+
 module.exports = db;
